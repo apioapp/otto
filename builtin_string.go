c81dfc200150d01885579b5177a16d118787a034
Sam Ban <sam@gpmd.co.uk>
2022-08-30 19:07:44 +0100

    add startsWith and endsWith

--- builtin_string.go
+++ builtin_string.go
@@ -453,6 +453,20 @@ func builtinString_toUpperCase(call FunctionCall) Value {
 	return toValue_string(strings.ToUpper(call.This.string()))
 }
 
+func builtinString_startsWith(call FunctionCall) Value {
+	checkObjectCoercible(call.runtime, call.This)
+	value := call.This.string()
+	target := call.Argument(0).string()
+	return toValue_bool(strings.HasPrefix(value, target))
+}
+
+func builtinString_endsWith(call FunctionCall) Value {
+	checkObjectCoercible(call.runtime, call.This)
+	value := call.This.string()
+	target := call.Argument(0).string()
+	return toValue_bool(strings.HasSuffix(value, target))
+}
+
 // 7.2 Table 2 — Whitespace Characters & 7.3 Table 3 - Line Terminator Characters
 const builtinString_trim_whitespace = "\u0009\u000A\u000B\u000C\u000D\u0020\u00A0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u2028\u2029\u202F\u205F\u3000\uFEFF"
 
--- inline.go
+++ inline.go
@@ -2127,6 +2127,52 @@ func _newContext(runtime *_runtime) {
 				call: builtinString_fromCharCode,
 			},
 		}
+		startsWith_function := &_object{
+			runtime:     runtime,
+			class:       "Function",
+			objectClass: _classObject,
+			prototype:   runtime.global.FunctionPrototype,
+			extensible:  true,
+			property: map[string]_property{
+				"length": _property{
+					mode: 0,
+					value: Value{
+						kind:  valueNumber,
+						value: 1,
+					},
+				},
+			},
+			propertyOrder: []string{
+				"length",
+			},
+			value: _nativeFunctionObject{
+				name: "startsWith",
+				call: builtinString_startsWith,
+			},
+		}
+		endsWith_function := &_object{
+			runtime:     runtime,
+			class:       "Function",
+			objectClass: _classObject,
+			prototype:   runtime.global.FunctionPrototype,
+			extensible:  true,
+			property: map[string]_property{
+				"length": _property{
+					mode: 0,
+					value: Value{
+						kind:  valueNumber,
+						value: 1,
+					},
+				},
+			},
+			propertyOrder: []string{
+				"length",
+			},
+			value: _nativeFunctionObject{
+				name: "endsWith",
+				call: builtinString_endsWith,
+			},
+		}
 		runtime.global.StringPrototype = &_object{
 			runtime:     runtime,
 			class:       classString,
@@ -2245,6 +2291,20 @@ func _newContext(runtime *_runtime) {
 					value: Value{
 						kind:  valueObject,
 						value: toUpperCase_function,
+					},
+				},
+				"startsWith": _property{
+					mode: 0101,
+					value: Value{
+						kind:  valueObject,
+						value: startsWith_function,
+					},
+				},
+				"endsWith": _property{
+					mode: 0101,
+					value: Value{
+						kind:  valueObject,
+						value: endsWith_function,
 					},
 				},
 				"substr": _property{
